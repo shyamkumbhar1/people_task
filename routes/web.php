81bc492121ee39020e44ba168ce453966a75543f
shyamkumbhar1 <78096242+shyamkumbhar1@users.noreply.github.com>
2024-04-13 18:36:28 +0530

    Create User Role Functionalty

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +11,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+
+});
+
+
