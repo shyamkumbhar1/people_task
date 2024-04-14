4f9dde76381cb5bc0dda504accb34ce15fee71d2
shyamkumbhar1 <78096242+shyamkumbhar1@users.noreply.github.com>
2024-04-15 03:55:48 +0530

    Create User Seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+    }
+}
